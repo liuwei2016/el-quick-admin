7ff815ca3e1f32b1ec381822b95f802e1f6114b2
liuwei2015 <liuwei2015@daojia-inc.com>
2021-10-17 20:32:21 +0800

    feat: 修改quick-table

--- packages/quick-table/src/quick-table.vue
+++ packages/quick-table/src/quick-table.vue
@@ -5,9 +5,9 @@
     class="quick-table"
     :style="{ height: styles.containerHeight }"
   >
-    <div class="quickOper">
+    <!-- <div class="quickOper">
       快捷操作9
-    </div>
+    </div> -->
     <!-- el-table -->
     <el-table
       ref="table"
