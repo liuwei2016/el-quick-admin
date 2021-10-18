1bc035a2041eb2502562b351329f7e45e67a5ddf
liuwei2015 <liuwei2015@daojia-inc.com>
2021-10-18 12:41:53 +0800

    feat: 设置esllint

---
+++ examples/src/views/login/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <vue-como-login></vue-como-login>
+</template>
+<script>
+import Vue from "vue";
+import vueComoLogin from "vue-como-login";
+Vue.use(vueComoLogin);
+import "vue-como-login/vue-como-login.css";
+export default {
+  components: {
+    // vueComoLogin
+  }
+};
+</script>
